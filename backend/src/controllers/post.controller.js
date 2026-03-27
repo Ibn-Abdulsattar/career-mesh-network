6598a605655b39b1e65b50264c42113858386fa6
Usman Chohan <attariusman813@gmail.com>
2026-03-27 11:07:36 +0500

    implemet CRUD on Post

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import { Post } from "../models/index.js";
+import ExpressError from "../utils/expressError.js";
 
 export const createPost = async (req, res, next) => {
   const user_id = req.user.user_id;
@@ -16,12 +17,53 @@ export const createPost = async (req, res, next) => {
     }
   }
 
-  await Post.create({
-    active,
+  if (!content && !mediaUrl) {
+    return next(
+      new ExpressError(
+        "Post cannot be empty! Please provide text or media.",
+        400,
+      ),
+    );
+  }
+
+  const newPost = await Post.create({
+    active: active ?? true,
     body: content,
     user_id,
     fileType: fileType,
     media: mediaUrl,
     likes: 0,
   });
+
+  return res
+    .status(201)
+    .json({ message: "New Post created successfully!", data: newPost });
+};
+
+export const getAllPosts = async (req, res, next) => {
+  const userId = req.user.user_id;
+  const allPosts = await Post.findAll({
+    where: { user_id: userId, active: true },
+  });
+
+  return res.status(200).json({ data: allPosts });
+};
+
+export const deletePost = async (req, res, next) => {
+  const userId = req.user.user_id;
+  const { id } = req.params;
+
+  const post = await Post.findByPk(id);
+
+  if (!post) {
+    return next(new ExpressError("No Record found!", 400));
+  }
+
+  if (post.user_id.toString() !== userId.toString()) {
+    return next(new ExpressError("Unauthorized to delete this post", 400));
+  }
+
+  await post.update({ active: false });
+
+  return res.status(200).json({ message: "Post deleted successfully!" });
 };
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import wrapAsync from "../utils/wrapAsync.js";
 import auth from "../middlewares/auth.js";
-import { createPost } from "../controllers/post.controller.js";
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+} from "../controllers/post.controller.js";
 import upload from "../services/upload.js";
 const router = Router();
 
-router.route("/").post(auth(["user"]), upload.single("media") , wrapAsync(createPost));
+router
+  .route("/")
+  .post(auth(["user"]), upload.single("media"), wrapAsync(createPost))
+  .get(auth(["user"]), wrapAsync(getAllPosts));
+router.route("/delete/:id").delete(auth(["user"]), wrapAsync(deletePost));
 
 export default router;
